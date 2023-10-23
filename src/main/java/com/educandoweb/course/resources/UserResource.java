236a885e8d1b4fa6c9a24e49311ef4fb67468fad
victorsousalima <victorsousa16042005@gmail.com>
2023-10-23 15:06:11 -0300

    User insert

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -4,11 +4,9 @@ import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
 import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +27,11 @@ public class UserResource {
     public ResponseEntity<User> findBydId(@PathVariable Long id) {
         User user = service.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping()
+    public ResponseEntity<User> insert(@RequestBody User user) {
+        User userSave = service.insert(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userSave);
     }
 }
